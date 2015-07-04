ea62b2ff740d97b4463269a81e9a1300c5df985f
Porfírio Ribeiro <porfirioribeiro@gmail.com>
2015-07-04 01:56:01 +0100

    Fix source links

--- tools/gendocs.sh
+++ tools/gendocs.sh
@@ -6,7 +6,7 @@ haxe doc.hxml
 
 cd ..
 
-haxelib run dox -i tools/xml -o docs/api -in macrox -ex macrox.test --toplevel-package macrox -D version 0.0.1 --title "MacroX - Macro eXtended tools!" -theme tools/doxtheme -D source-path https://github.com/porfirioribeiro/MacroX/blob/development/src/
+haxelib run dox -i tools/xml -o docs/api -in macrox -ex macrox.test --toplevel-package macrox -D version 0.0.1 --title "MacroX - Macro eXtended tools!" -theme tools/doxtheme -D source-path https://github.com/porfirioribeiro/MacroX/blob/master/src/
 
 cd tools
 
